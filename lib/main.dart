137308bbd0e8dbfe3910b81c9205f8422f671740
英吾 <mono.eigo@gmail.com>
2021-10-29 16:40:39 +0900

    UI変更

---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CartCheck(),
+    );
+  }
+}
+
+//この下がHirokiくんの書いたコード＋コード追加部分
+
+class CartCheck extends StatefulWidget {
+  const CartCheck({Key? key}) : super(key: key);
+
+  @override
+  _CartCheckState createState() => _CartCheckState();
+}
+
+class _CartCheckState extends State<CartCheck> {
+  @override
+  Widget build(BuildContext context) {
+    List<String> list = [
+      "meat",
+      "detergent",
+      "chicken",
+      "USB",
+      "sponge",
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Confirmation'),
+        backgroundColor: Color(0xff254284),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ListView.builder(
+              padding: EdgeInsets.only(
+                bottom: 30,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return _messageItem(
+                  list[index],
+                );
+              },
+              itemCount: list.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget separatorItem() {
+    return Container(
+      height: 10,
+      color: Colors.white,
+    );
+  }
+
+  //Hirokiくんの書いたコード
+
+  // Widget _messageItem(String title) {
+  //   return Card(
+  //     child: Column(
+  //       children: <Widget>[
+  //         ListTile(
+  //           title: Text(
+  //             title,
+  //             style: TextStyle(color: Colors.black, fontSize: 18.0),
+  //           ),
+  //           subtitle: Text("color/size"),
+  //         ),
+  //       ], // 長押し
+  //     ),
+  //   );
+  // }
+
+  //後述したコード
+  Widget _messageItem(String title) {
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  ),
+                ),
+                Text('color/size'),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 50,
+                  top: 20,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '0',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          height: 40,
+                          color: Colors.yellow[700],
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '−',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 40,
+                          color: Colors.yellow[700],
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '+',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
